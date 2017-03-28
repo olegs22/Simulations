976944f5be83be6a83e03dee12e17231e8d544d0
Olegs <Oleg@MacBook-Pro-de-Oleg.local>
2017-03-27 19:36:43 -0600

    fixed a lood index

--- ponit2_c_v2.c
+++ ponit2_c_v2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <time.h>
 
 
-double * corr_2p(int len_DD, double *pos_x, double *pos_y, double *pos_z, int limit, int bin){
+double *corr_2p(int len_DD, double *pos_x, double *pos_y, double *pos_z, int limit, int bin){
 
   int i,j,k,l,r;
   double RR[len_DD][3];
@@ -11,18 +12,25 @@ double * corr_2p(int len_DD, double *pos_x, double *pos_y, double *pos_z, int li
   double r_data[3];
   double DD_r, RR_r, DR_r;
   double coords[3];
-  double range = RAND_MAX/limit;
-  double max_d = sqrt(2.0*pow(limit,2.0));
+  //double range = RAND_MAX/limit;
+  double max_d = sqrt(3.0)*limit;
   double bin_width = (double)(max_d/bin);
   double hist0[bin];
   double hist1[bin];
   double hist2[bin];
   static double corr[10];
 
+  srand(12345);
+
   for (i=0; i<len_DD; i++){
+    /*
     coords[0] = rand() / range;
     coords[1] = rand() / range;
     coords[2] = rand() / range;
+    */
+    coords[0] = drand48()*limit;
+    coords[1] = drand48()*limit;
+    coords[2] = drand48()*limit;
 
     r_data[0] = pos_x[i];
     r_data[1] = pos_y[i];
@@ -36,24 +44,26 @@ double * corr_2p(int len_DD, double *pos_x, double *pos_y, double *pos_z, int li
 
 
   for (i=0; i<len_DD; i++){
-    for (k=1; k<len_DD; k++){
+    for (k=i+1; k<len_DD; k++){
 
-      DD_r = sqrt(pow(DD[i][0]-DD[k][0],2.0) + pow(DD[i][1]-DD[k][1],2.0) + pow(DD[i][2]-DD[k][2],2.0));
-      RR_r = sqrt(pow(RR[i][0]-RR[k][0],2.0) + pow(RR[i][1]-RR[k][1],2.0) + pow(RR[i][2]-RR[k][2],2.0));
-      DR_r = sqrt(pow(DD[i][0]-RR[k][0],2.0) + pow(DD[i][1]-RR[k][1],2.0) + pow(DD[i][2]-RR[k][2],2.0));
+      DD_r = sqrt(pow(DD[k][0]-DD[i][0],2) + pow(DD[k][1]-DD[i][1],2) + pow(DD[k][2]-DD[i][2],2));
+      RR_r = sqrt(pow(RR[k][0]-RR[i][0],2) + pow(RR[k][1]-RR[i][1],2) + pow(RR[k][2]-RR[i][2],2));
+      DR_r = sqrt(pow(DD[k][0]-RR[i][0],2) + pow(DD[k][1]-RR[i][1],2) + pow(DD[k][2]-RR[i][2],2));
     }
 
     for (l=0; l<bin; l++){
-      if(DD_r>=(double)(l*bin_width) && DD_r<(double)((l+1)*bin_width)){
+      if(DD_r>=bin_width*l && DD_r<bin_width*(l+1)){
         hist0[l] += 1;
       }
-      if(RR_r>=(double)(l*bin_width) && RR_r<(double)((l+1)*bin_width)){
+      if(RR_r>=bin_width*l && RR_r<bin_width*(l+1)){
         hist1[l] += 1;
       }
-      if(DR_r>=(double)(l*bin_width) && DR_r<(double)((l+1)*bin_width)){
+      if(DR_r>=bin_width*l && DR_r<bin_width*(l+1)){
         hist2[l] += 1;
       }
     }
+
+
   }
 
 
@@ -69,35 +79,4 @@ double * corr_2p(int len_DD, double *pos_x, double *pos_y, double *pos_z, int li
 
   return corr;
 
-}
-
-int main(){
-  FILE *file;
-  //int len = 3102;
-  int len = 10000;
-  double box_size = 50.0;
-  int bining = 10;
-  double *hist;
-  double x[len];
-  double y[len];
-  double z[len];
-  double pos1[len], pos2[len], pos3[len];
-  int i,k,j;
-
-  //file = fopen("/Users/Oleg/Documents/Simulations/positions.txt","r");
-  file = fopen("/Users/Oleg/Documents/Simulations/random_data.txt","r");
-
-
-  //while (!feof(file)) {
-  for(k=0; k<len; k++){
-    /* code */
-    fscanf(file,"%lf\t%lf\t%lf",&x[k],&y[k],&z[k]);
-  }
-
-  hist = corr_2p(len+1, x, y, z, box_size, bining);
-  for (i=0; i<bining; i++){
-    printf("*(hist + %d) : %lf\n", i, *(hist+i));
-  }
-  fclose(file);
-  return 0;
 }
