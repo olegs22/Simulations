6fd4c8766c8f1acd7e6cdf9eaf931edca1aabee3
Olegs <Oleg@MacBook-Pro-de-Oleg.local>
2017-04-04 23:12:48 -0500

    Now it frees the memory of the results and new functions

--- corr2.py
+++ corr2.py
@@ -29,9 +29,40 @@ def corr2(data, box_size, bins, model):
     y = data[:,1]
     z = data[:,2]
 
-    max_distance = np.sqrt(3.0)*float(box_size)
-    binning = np.linspace(0.0, max_distance, bins)
+    result = np.zeros(bins)
 
     c_function = _corr_2p(len(x), x, y, z, box_size, bins, model)
 
-    return binning, c_function
+    for i in range(bins):
+        result[i] = c_function[i]
+    _mod.free(c_function)
+    return result
+
+_corr_2p_norm =_mod.corr_2p_norm
+_corr_2p_norm.argtypes = (ctypes.c_int, ctypes.c_int, DoubleArray, DoubleArray,DoubleArray,\
+                        DoubleArray, DoubleArray, DoubleArray, ctypes.c_int, ctypes.c_int,\
+                        ctypes.c_int)
+_corr_2p_norm.restype = ctypes.POINTER(ctypes.c_double)
+
+def corr_2p_n(positions, randoms, box_size, bins, model):
+    p_x = positions[:,0]
+    p_y = positions[:,1]
+    p_z = positions[:,2]
+
+    r_x = randoms[:,0]
+    r_y = randoms[:,1]
+    r_z = randoms[:,2]
+
+    result = np.zeros(bins)
+
+    function = _corr_2p_norm(len(p_x), len(r_x), p_x, p_y, p_z, r_x, r_y, r_z,\
+                            box_size, bins, model)
+    for i in range(bins):
+        result[i] = function[i]
+    _mod.free(function)    
+    return result
+
+def binning(box_size, bins):
+    max_distance = np.sqrt(3.0) * box_size
+    b= np.linspace(0.0, max_distance, bins)
+    return b
